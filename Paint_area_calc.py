4fce3373d14879fef1da3c9d2780d8589b023b04
Exlare <78602187+Exlare@users.noreply.github.com>
2022-07-21 23:19:52 +0200

    Add files via upload

---
+++ Paint_area_calc.py
@@ -0,0 +1,13 @@
+#Paint Area Calculator
+import math
+
+def paint_calc(height, width, cover):
+    area = height * width
+    numb_of_cans = math.ceil(area / cover)
+    print(f"You'll need {numb_of_cans} cans of paint.")
+
+
+test_h = int(input("Height of wall: "))
+test_w = int(input("Width of wall: "))
+coverage = int(input("How many squaremeters can one can cover?: "))
+paint_calc(height=test_h, width=test_w, cover=coverage)
